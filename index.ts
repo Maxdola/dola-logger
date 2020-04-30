cb5e0cc81d037c41b2d1d618159bafd2c4cce1e3
Max Hardtke <Max-Tom@outlook.de>
2020-05-01 00:48:39 +0200

    Npm Package cleanup

--- index.ts
+++
@@ -1,9 +0,0 @@
-import LoggerGroup from "./src/LoggerGroup";
-import Logger from "./src/Logger";
-import Log from "./src/Log";
-
-module.exports = {
-    LoggerGroup,
-    Logger,
-    Log
-}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import LoggerGroup from "./LoggerGroup";
+import Logger from "./Logger";
+import Log from "./Log";
+
+module.exports = {
+    LoggerGroup,
+    Logger,
+    Log
+}
