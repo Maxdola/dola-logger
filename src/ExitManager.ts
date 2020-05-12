dd274e593d751f64a11d4dc8c463cabd0d9649e2
Max Hardtke <Max-Tom@outlook.de>
2020-05-12 16:43:49 +0200

    ~ Implemented missed hook to process#exit

--- src/ExitManager.ts
+++ src/ExitManager.ts
@@ -7,6 +7,12 @@ export const addExitFunction = (ef : ExitFunction) => {
     exitFunctions.push(ef);
 }
 
+process.on("exit", () => {
+    exitFunctions.forEach(ef => {
+        ef(false);
+    })
+})
+
 export let defaultLogDir = path.join(process.cwd(), "logs") + "\\";
 export const setLogDir = dirPath => {
     defaultLogDir = path.resolve(dirPath);
